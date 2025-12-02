dfdb1cdec0841798c034251d04ba300d29e1508b
IoannisLykomitros <167572966+IoannisLykomitros@users.noreply.github.com>
2025-12-02 21:14:54 +0200

    implement Wordle game structure with random word selection and initial word list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,20 @@
-const App = () => {
+import { useEffect, useState, type JSX } from 'react'
+import words from '../words'; 
 
+const WordleApp = (): JSX.Element => {
+
+  const [wordToGuess, setWordToGuess] = useState<string>('');
+
+  useEffect((): void => {
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    setWordToGuess(randomWord);
+  }, []);
+  
   return (
     <>
-      <h1>WORDLE</h1>
+      <h1>{wordToGuess}</h1>
     </>
   )
 } 
 
-export default App
+export default WordleApp
